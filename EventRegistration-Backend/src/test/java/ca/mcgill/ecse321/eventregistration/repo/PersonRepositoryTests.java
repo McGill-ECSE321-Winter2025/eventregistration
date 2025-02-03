e2e1080ba2dde96743f542c9ec302a4a6573b3e5
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-02-03 18:03:28 -0500

    Write basic test for Person persistence

---
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/repo/PersonRepositoryTests.java
@@ -0,0 +1,41 @@
+package ca.mcgill.ecse321.eventregistration.repo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.eventregistration.model.Person;
+
+@SpringBootTest
+public class PersonRepositoryTests {
+	@Autowired
+	private PersonRepository repo;
+
+	@Test
+	public void testCreateAndReadPerson() {
+		// Arrange
+		Date today = Date.valueOf("2025-02-03");
+		Person peteMikeJoe = new Person(
+				"Pete Mike Joe",
+				"pete.mike.joe@mail.mcgill.ca",
+				"petemjdabest",
+				today);
+		peteMikeJoe = repo.save(peteMikeJoe);
+
+		// Act
+		Person peteMikeJoeFromDb = repo.findPersonById(peteMikeJoe.getId());
+
+		// Assert
+		assertNotNull(peteMikeJoeFromDb);
+		assertEquals(peteMikeJoe.getId(), peteMikeJoeFromDb.getId());
+		assertEquals(peteMikeJoe.getName(), peteMikeJoeFromDb.getName());
+		assertEquals(peteMikeJoe.getEmailAddress(), peteMikeJoeFromDb.getEmailAddress());
+		assertEquals(peteMikeJoe.getPassword(), peteMikeJoeFromDb.getPassword());
+		assertEquals(peteMikeJoe.getCreationDate(), peteMikeJoeFromDb.getCreationDate());
+	}
+}
