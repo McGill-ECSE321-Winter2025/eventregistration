50312851826a88fd1c7eb40ccfb7f528fdba99b3
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-02-19 16:12:01 -0500

    Use @Transactional

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/service/PersonService.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/service/PersonService.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import ca.mcgill.ecse321.eventregistration.model.Person;
 import ca.mcgill.ecse321.eventregistration.repo.PersonRepository;
@@ -14,6 +15,7 @@ public class PersonService {
 	@Autowired
 	private PersonRepository personRepo;
 
+	@Transactional
 	public Person createPerson(String name, String email, String password) {
 		Date today = Date.valueOf(LocalDate.now());
 		Person p = new Person(name, email, password, today);
