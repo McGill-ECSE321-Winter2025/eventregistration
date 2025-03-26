1b5d39f47806dc3c65aaf38499ec103dd7873a67
ByronChen12138 <Byron.chen12138@gmail.com>
2025-03-26 16:39:01 -0400

    feat: Directives applied & authStore

--- EventRegistration-Frontend/src/components/UserProfile.vue
+++ EventRegistration-Frontend/src/components/UserProfile.vue
@@ -1,16 +1,30 @@
 <script setup>
 
+import {useAuthStore} from "@/stores/authStore.js";
+
 const props = defineProps( {
   username: String,
   userEmail: String,
 });
 
+const authStore = useAuthStore();
+
+function loginAction() {
+  authStore.login(props.username, props.userEmail, 1);
+}
+
+function logoutAction() {
+  authStore.logout();
+}
+
 </script>
 
 <template>
   <div class="user-profile">
-    <p id="user-name"> {{ username }} </p>
-    <p id="user-email"> {{ userEmail }} </p>
+    <button v-if="!authStore.user.isAuthenticated" @click="loginAction()"> Login </button>
+    <button v-else @click="logoutAction()"> Logout </button>
+    <p id="user-name"> {{ authStore.user.username }} </p>
+    <p id="user-email"> {{ authStore.user.userEmail }} </p>
   </div>
 </template>
 
